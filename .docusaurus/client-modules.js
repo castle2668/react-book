9efefdef4e588c819e5cbc4e620c460de51de2ce
Sealman <castle2668@gmail.com>
2022-08-30 09:43:18 +0800

    fix: delete ignored files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/sean/Desktop/Sealman/react-book/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/sean/Desktop/Sealman/react-book/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/sean/Desktop/Sealman/react-book/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/sean/Desktop/Sealman/react-book/src/css/custom.css'),
-];
